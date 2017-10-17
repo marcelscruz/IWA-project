474d8d467c3a70b4b627e18bc8edec6982818a7b
Marcel Cruz <marcelcruz@Marcels-MacBook-Pro.local>
2017-10-17 19:26:38 +0100

    login fixed, file names changed

--- js/load-views.js
+++ js/load-views.js
@@ -1,21 +1,19 @@
-console.log('script.js works!');
-
 $.get( "../login.html", function( data ) {
   $( "#login" ).html( data );
 });
 
-$.get( "../view1.html", function( data ) {
-  $( "#view-1" ).html( data );
+$.get( "../home.html", function( data ) {
+  $( "#home" ).html( data );
 });
 
-$.get( "../view2.html", function( data ) {
-  $( "#view-2" ).html( data );
+$.get( "../new-story.html", function( data ) {
+  $( "#new-story" ).html( data );
 });
 
-$.get( "../view3.html", function( data ) {
-  $( "#view-3" ).html( data );
+$.get( "../account.html", function( data ) {
+  $( "#account" ).html( data );
 });
 
-$.get( "../view4.html", function( data ) {
-  $( "#view-4" ).html( data );
+$.get( "../json.html", function( data ) {
+  $( "#json" ).html( data );
 });
