c11328ea9a8960f03ea396cfb50e840dfc865149
Marcel Cruz <marcelcruz@Marcels-MacBook-Pro.local>
2017-11-09 15:11:43 +0000

    news showing in all tabs! yeyy

--- js/news-api.js
+++ js/news-api.js
@@ -1,28 +1,40 @@
 // apiKey 5ad08b376dc24ce8b8fbc2a1abcbd1c3
 
-var BBCArticle = [];
+function getNews (news, selector){
+  $.each(news, function(i, value){
+    $(selector).append(
+      ` <li class='card'>
+          <a href='#'>
+            <div class='card-header'>
+              <img src='${value.urlToImage}' width='100%'>
+            </div>
+            <div class='card-content'>
+              <div class='card-content-inner'>${value.title}</div>
+            </div>
+        </a>
+        </li>
+      `
+    );
+  });
+}
 
-$$.getJSON('https://newsapi.org/v1/articles?source=bbc-news&sortBy=top&apiKey=5ad08b376dc24ce8b8fbc2a1abcbd1c3',
+$$.getJSON('https://newsapi.org/v1/articles?source=bbc-sport&sortBy=top&apiKey=5ad08b376dc24ce8b8fbc2a1abcbd1c3',
   function (data) {
-
-    for (var key in data.articles) {
-
-      BBCArticle[key] = data.articles[key];
-    }
-    // console.log(BBCArticle[8]);
-    // BBCArticle.map(function (x) {
-    //   console.log(BBCArticle[x].title);
-    // });
+    let bbcSports = data.articles;
+    getNews(bbcSports, '#sports-cards');
   }
 );
 
-{/* <div class='list-block cards-list'>
-  <ul>
-    <li class='card'>
-      <div class='card-header'>Card Header</div>
-      <div class='card-content'>
-        <div class='card-content-inner'>Card content</div>
-      </div>
-    </li>
-  </ul>
-</div> */}
+$$.getJSON('https://newsapi.org/v1/articles?source=business-insider&sortBy=top&apiKey=5ad08b376dc24ce8b8fbc2a1abcbd1c3',
+  function (data) {
+    let businessInsider = data.articles;
+    getNews(businessInsider, '#business-cards');
+  }
+);
+
+$$.getJSON('https://newsapi.org/v1/articles?source=financial-times&sortBy=top&apiKey=5ad08b376dc24ce8b8fbc2a1abcbd1c3',
+  function (data) {
+    let financialTimes = data.articles;
+    getNews(financialTimes, '#financial-cards');
+  }
+);
